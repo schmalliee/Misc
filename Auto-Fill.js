dded5f0325bf4d4eb89a14161546b55aabe03229
Allie Craig <schmalliee@gmail.com>
2018-09-10 12:03:29 -0600

    Update Auto-Fill.js

--- Auto-Fill.js
+++ Auto-Fill.js
@@ -4,6 +4,7 @@
   var mainLoadable;
   
   //load jquery
+  console.log("Load JQuery");
   var script = document.createElement('script');
   script.src = '//code.jquery.com/jquery-1.11.0.min.js';
   document.getElementsByTagName('head')[0].appendChild(script);

767fecfba8664018acc4637568be226fbe3615ff
Allie Craig <schmalliee@gmail.com>
2018-10-11 12:13:33 -0600

    Update Gig-Auto.js

--- Gig-Auto.js
+++ Gig-Auto.js
@@ -87,6 +87,9 @@
         w.setTimeout(doSomeSubmitting,1300);
       } if(name == 'price'){
           $(cards.state.lastCard).find('#price-footer').find('input[type="submit"]').first().click();      
+      } if(name == 'account'){
+          $(cards.state.lastCard).find('form').find('input[type="password"]').first()[0].value = "temp123";
+          $(cards.state.lastCard).find('form').find('input[type="submit"]').first().click();      
       } else { 
         //if(name == 'combinedSubmit') doneSubmitting = true;
         $(cards.state.lastCard).find('form').find('input[type="submit"]').first().click();

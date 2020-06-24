7955249ae28c11558308f0de40ce61de2caf748b
Allie Craig <schmalliee@gmail.com>
2020-06-24 13:03:30 -0600

    Update FppAtZip.js

--- FppAtZip.js
+++ FppAtZip.js
@@ -1,38 +1,54 @@
+const waitForEl = function(selector, callback) {
+  if ($(selector).length) {
+    callback();
+  } else {
+    setTimeout(() => {
+      waitForEl(selector, callback);
+    }, 100);
+  }
+};
+
 const clickThrough = function() {
   $('.nextPage').click();
   getURL();
 };
 
 const fillAddress = function() {
-  const address = $('.address-input[data-test="street-input"] > [data-qa="text-input"]');
-  const city = $('.address-input[data-test="city-input"] > [data-qa="text-input"]');
-  const zip = $('.address-input[data-test="zip-input"] > [data-qa="text-input"]');
+  const address = $('.page-header');
+  waitForEl(address, () => {
+    const address = $('.address-input[data-test="street-input"] > [data-qa="text-input"]');
+    const city = $('.address-input[data-test="city-input"] > [data-qa="text-input"]');
+    const zip = $('.address-input[data-test="zip-input"] > [data-qa="text-input"]');
 
-  address.value = '1234 Main St';
-  address.dispatchEvent(new Event('input'));
-  city.value = 'Denver';
-  city.dispatchEvent(new Event('input'));
-  const zipCode = prompt('Please enter a zip", "80202');
-  zip.value = zipCode;
-  zip.dispatchEvent(new Event('input'));
-  clickThrough();
+    address.value = '1234 Main St';
+    address.dispatchEvent(new Event('input'));
+    city.value = 'Denver';
+    city.dispatchEvent(new Event('input'));
+    const zipCode = prompt('Please enter a zip", "80202');
+    zip.value = zipCode;
+    zip.dispatchEvent(new Event('input'));
+    clickThrough();
+  });
 };
 
 const fillContact = function() {
-  const firstName = $('.contact-input[data-test="first-name-input"] > [data-qa="text-input"]');
-  const lastName = $('.contact-input[data-test="last-name-input"] > [data-qa="text-input"]');
-  const phone = $('.contact-input[data-test="phone-input"] > [data-qa="text-input"]');
-  const email = $('.contact-input[data-test="email-input"] > [data-qa="text-input"]');
+  const firstName = $('page-header');
+  waitForEl(firstName, () => {
+    const firstName = $('.contact-input[data-test="first-name-input"] > [data-qa="text-input"]');
+    const lastName = $('.contact-input[data-test="last-name-input"] > [data-qa="text-input"]');
+    const phone = $('.contact-input[data-test="phone-input"] > [data-qa="text-input"]');
+    const email = $('.contact-input[data-test="email-input"] > [data-qa="text-input"]');
 
-  firstName.value = 'Wade';
-  firstName.dispatchEvent(new Event('input'));
-  lastName.value = 'Wilson';
-  lastName.dispatchEvent(new Event('input'));
-  phone.value = randomPhone();
-  phone.dispatchEvent(new Event('input'));
-  email.value = 'wwilson.' + Date.now() + '.fppatzip@edify.com';
-  email.dispatchEvent(new Event('input'));
-  clickThrough();
+    firstName.value = 'Wade';
+    firstName.dispatchEvent(new Event('input'));
+    lastName.value = 'Wilson';
+    lastName.dispatchEvent(new Event('input'));
+    phone.value = randomPhone();
+    phone.dispatchEvent(new Event('input'));
+    email.value = 'wwilson.' + Date.now() + '.fppatzip@edify.com';
+    email.dispatchEvent(new Event('input'));
+    clickThrough();
+  });
 };
 
 const randomPhone = function() {
